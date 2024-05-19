289f9189a1762f4b1d0778db7d51eccff1cf8949
Ansuman Mishra <66157597+Ansuman-mishra@users.noreply.github.com>
2024-05-20 05:01:29 +0530

    first commit

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUser()
+    {
+        var users = await _context.Users.ToListAsync();
+        if (users == null || !users.Any())
+            return NotFound("No users found.");
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUserById(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        return user;
+    }
+}
---
+++ API/Insert.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"ansuman");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"atman");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"silu");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4,"milu");
